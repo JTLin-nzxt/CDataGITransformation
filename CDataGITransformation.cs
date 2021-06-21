3fea5347ced1845cca36964a2ae465b45a5309eb
JTLin-nzxt <jt.lin@nzxt.com>
2021-06-21 10:36:28 +0800

    Create CDataGITransformation.cs

---
+++ CDataGITransformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace CDataGITransformation
+{
+    class CDataGITransformation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the xml folder path :");
+
+            //string filePath = Console.ReadLine();
+            string folderPath = Directory.GetCurrentDirectory();
+
+            string[] xmlFiles = Directory.GetFiles(folderPath, "*.xml", SearchOption.AllDirectories);
+            foreach (string xmlFile in xmlFiles)
+            {
+                Console.WriteLine(xmlFile);
+                CDataGITransforming(xmlFile);
+            }
+        }
+        public static void CDataGITransforming(string filePath) 
+        {
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(filePath);
+
+            // trans every GIResult.Caption to upper case
+            XmlNodeList giresultNodes = xmldoc.GetElementsByTagName("GIResult");
+            foreach (XmlNode giresultNode in giresultNodes)
+            {
+                string upperCaption = giresultNode.Attributes["Caption"].Value.ToUpper();
+                giresultNode.Attributes["Caption"].Value = upperCaption;
+            }
+
+            xmldoc.Save(filePath);
+        }
+    }
+}
